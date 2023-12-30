ed78833f9d803a426d8af967b2e0fe79ad68d6ed
Tareq <tareqnmd@gmail.com>
2023-12-30 12:20:14 +0600

    update

---
+++ components/SendBasicEmail.tsx
@@ -0,0 +1,30 @@
+'use client';
+import { Resend } from 'resend';
+
+const SendBasicEmail = () => {
+	const resend = new Resend('re_Vt7A7Lnq_EC5tLPHWdg3QZMVFK2M7goim');
+	const send_mail = async () => {
+		const { data, error } = await resend.emails.send({
+			from: 'Acme <onboarding@resend.dev>',
+			to: ['tareqatoffice@gmail.com'],
+			subject: 'Hello World',
+			html: '<strong>It works!</strong>',
+		});
+
+		if (error) {
+			return console.error({ error });
+		}
+
+		console.log({ data });
+	};
+	return (
+		<button
+			className="basic-mail-send-btn"
+			onClick={send_mail}
+		>
+			Send a basic Email
+		</button>
+	);
+};
+
+export default SendBasicEmail;
