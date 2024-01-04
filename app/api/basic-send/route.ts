f44ff8d8653c1af288e6c5431c820329dc7fc4a3
Tareq <tareqnmd@gmail.com>
2024-01-04 15:43:12 +0600

    send multiple send added

--- app/api/basic-send/route.ts
+++ app/api/basic-send/route.ts
@@ -3,13 +3,15 @@ import { Resend } from 'resend';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-export async function POST() {
+export async function POST({ firstName }: { firstName: string }) {
 	try {
 		const data = await resend.emails.send({
-			from: 'Acme <onboarding@resend.dev>',
+			from: 'no-reply@tareqnmd.com',
 			to: ['tareqatoffice@gmail.com'],
-			subject: 'Hello world',
-			react: EmailTemplate({ firstName: 'John' }),
+			bcc: ['tareqatoffice@gmail.com'],
+			cc: ['tareqatoffice@gmail.com'],
+			subject: 'Basic Email Send',
+			react: EmailTemplate({ firstName }),
 		});
 
 		return Response.json(data);
---
+++ app/api/multiple-send/route.ts
@@ -0,0 +1,24 @@
+import { EmailTemplate } from '@/components/email/basic-template';
+import { Resend } from 'resend';
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+export async function POST() {
+	try {
+		const emails = ['tareqnmd@gmail.com', 'tareqatoffice@gmail.com'];
+		const unique_emails = [...new Set(emails)];
+		const emailPromises = unique_emails.map((email) => {
+			return resend.emails.send({
+				from: 'no-reply@tareqnmd.com',
+				to: email,
+				subject: 'Multiple Email Send',
+				react: EmailTemplate({ firstName: 'John' }),
+			});
+		});
+		const responses = await Promise.all(emailPromises);
+
+		return Response.json({ status: 200, responses });
+	} catch (error) {
+		return Response.json({ error });
+	}
+}
